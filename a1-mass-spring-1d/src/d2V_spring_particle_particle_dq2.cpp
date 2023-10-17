baef1457020bfdc872d84798c21d308b5e66cfe1
liumu96 <918883717@qq.com>
2023-10-17 18:09:35 +0800

    forward_euler method

--- a1-mass-spring-1d/src/d2V_spring_particle_particle_dq2.cpp
+++ a1-mass-spring-1d/src/d2V_spring_particle_particle_dq2.cpp
@@ -1,6 +1,7 @@
 #include <d2V_spring_particle_particle_dq2.h>
 
-void d2V_spring_particle_particle_dq2(Eigen::MatrixXd &H, const Eigen::VectorXd &q, double stiffness) {
-    H.resize(1,1);
-    
+void d2V_spring_particle_particle_dq2(Eigen::MatrixXd &H, const Eigen::VectorXd &q, double stiffness)
+{
+    H.resize(1, 1);
+    H << stiffness;
 }
--- a1-mass-spring-1d/src/dV_spring_particle_particle_dq.cpp
+++ a1-mass-spring-1d/src/dV_spring_particle_particle_dq.cpp
@@ -1,6 +1,7 @@
 #include <dV_spring_particle_particle_dq.h>
 
-void dV_spring_particle_particle_dq(Eigen::VectorXd &dV, const Eigen::VectorXd &q, double stiffness) {
+void dV_spring_particle_particle_dq(Eigen::VectorXd &dV, const Eigen::VectorXd &q, double stiffness)
+{
     dV.resize(1);
-    
+    dV = stiffness * q;
 }
